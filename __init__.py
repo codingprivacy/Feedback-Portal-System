0bfe82df45a9b3e5e383f5b96f1f466b0b19b2f7
harsh patel <harshpatel44@github.com>
2019-03-06 22:44:59 +0530

    all files organised

---
+++ __init__.py
@@ -0,0 +1,29 @@
+from flask import Flask,request
+from flask_socketio  import SocketIO, emit
+from backend import main
+
+app = Flask(__name__)
+
+
+
+socketio = SocketIO(app)
+@socketio.on('start_event')
+def handleMessage(msg):
+    print("message",msg)
+    question=main.get_question(request.sid,"form1")
+    #print(question)
+    emit('my_response',question)
+
+@socketio.on('send_answar')
+def question(msg):
+    #print(msg)
+    main.save_answar(request.sid,msg,"form1")    #taking answar
+    question=main.get_question(request.sid,"form1")    #sending question
+    #print(question)
+    emit('get_question',question)
+if __name__ == '__main__':
+    socketio.run(app)
+
+
+
+
